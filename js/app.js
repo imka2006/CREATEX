d7ebac149fd8c671c0cf9d4f3e405ff26b7e7ff9
sexyboyfrend <31unknown12@gmail.com>
2022-06-30 23:39:24 +0600

    added new section hero, hero  not ready

--- js/app.js
+++ js/app.js
@@ -17,3 +17,26 @@ burger__delete.addEventListener("click", () => {
     burger__menu.classList.remove("active")
     body.style.overflow = 'unset'
 })
+
+const hero__swiper = new Swiper(".hero__swiper", {
+    loop: true,
+    // spaceBetween: 10,
+    navigation: {
+      nextEl: ".hero__next--button",
+      prevEl: ".hero__prev--button",
+    },
+  });
+  
+  
+  const hero__swiper__second = new Swiper(".hero__swiper__second", {
+    loop: true,
+    spaceBetween: 10,
+        slidesPerView: 4,
+        freeMode: true,
+        watchSlidesProgress: true,
+    navigation: {
+        nextEl: ".hero__next--button",
+        prevEl: ".hero__prev--button",
+      },
+    spaceBetween: 0,
+  });
