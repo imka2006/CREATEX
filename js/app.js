ff1cb4a8da492821d14d8fccf5abfa13288c9b06
sexyboyfrend <31unknown12@gmail.com>
2022-06-30 15:27:43 +0600

    add

--- js/app.js
+++ js/app.js
@@ -3,10 +3,9 @@ const body = document.querySelector("body")
 const burger__delete = document.querySelector(".burger__delete")
 
 
-console.log(burger__menu.classList);
 burger__menu.addEventListener("click", () => {
     burger__menu.classList.toggle("active")
-
+    body.style.overflow = 'hidden'
     if (burger__menu.classList.contains("active")) {
         body.style.overflow = 'hidden'
     } else {
